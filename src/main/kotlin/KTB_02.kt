772a6962cc7ad05613de51871699b00a8052dd39
Anton <new_perspective@mail.ru>
2025-08-08 22:02:41 +0300

    KTB-02 create and reading file

--- src/main/kotlin/KTB_02.kt
+++ src/main/kotlin/KTB_02.kt
@@ -5,5 +5,5 @@ import java.io.File
 fun main() {
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.forEachLine { println(it) }
+    wordsFile.readLines().forEach { println(it) }
 }
