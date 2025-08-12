49953c40be58ebabc1dba722796cd12468e67879
Anton <new_perspective@mail.ru>
2025-08-12 10:35:09 +0300

    KTB-04 used only one file.kt. Rename class.

--- src/main/kotlin/ConsoleTrainer.kt
+++ src/main/kotlin/ConsoleTrainer.kt
@@ -2,18 +2,18 @@ package org.example
 
 import java.io.File
 
-data class Word1(
+data class Word(
     val origin: String,
     val translate: String,
-    var correctAnswersCount: Int? = null,
+    var correctAnswersCount: Int = 0,
 )
 
 fun main() {
-    val dictionary: MutableList<Word1> = mutableListOf()
+    val dictionary: MutableList<Word> = mutableListOf()
     val wordsFile: File = File("words.txt")
     wordsFile.forEachLine {
         val line: List<String> = it.split("|")
-        val word = Word1(
+        val word = Word(
             origin = line[0],
             translate = line[1],
             correctAnswersCount = line[2].toIntOrNull() ?: 0
