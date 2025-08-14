a127a15e383d624e7ccaaafb5fffcdd11370e2ea
Anton <new_perspective@mail.ru>
2025-08-14 12:46:31 +0300

    KTB-06 add 'fun displayStatistics' and implement it in menu

--- src/main/kotlin/ConsoleTrainer.kt
+++ src/main/kotlin/ConsoleTrainer.kt
@@ -2,15 +2,18 @@ package org.example
 
 import java.io.File
 
+const val NUMBER_OF_SUCCESS_TRIES: Int = 3
+const val ONE_HUNDRED_PERCENT: Double = 100.0
+
 data class Word(
     val origin: String,
     val translate: String,
     var correctAnswersCount: Int = 0,
 )
 
-fun main() {
+fun loadDictionary(fileName: String): List<Word> {
     val dictionary: MutableList<Word> = mutableListOf()
-    val wordsFile: File = File("words.txt")
+    val wordsFile: File = File(fileName)
     wordsFile.forEachLine {
         val line: List<String> = it.split("|")
         val word = Word(
@@ -20,5 +23,34 @@ fun main() {
         )
         dictionary.add(word)
     }
-    dictionary.forEach { println(it) }
+    return dictionary
+}
+
+fun displayStatistics(workingFile: List<Word>): String {
+    val totalCount: Int = workingFile.count()
+    val learnedCount: Int = workingFile.filter { it.correctAnswersCount >= NUMBER_OF_SUCCESS_TRIES }.count()
+    val percent: Double = learnedCount * ONE_HUNDRED_PERCENT / totalCount
+    return "Выучено $learnedCount из $totalCount слов | ${"%.0f".format(percent)}%"
+}
+
+fun main() {
+    val dictionary: List<Word> = loadDictionary("words.txt")
+    val statistics: String = displayStatistics(dictionary)
+
+    while (true) {
+        println(
+            "Меню: \n" +
+                    "1 – Учить слова\n" +
+                    "2 – Статистика\n" +
+                    "0 – Выход"
+        )
+        val userInput: Int? = readln().toIntOrNull()
+
+        when (userInput) {
+            1 -> println("Вы выбрали учить слова.")
+            2 -> println("$statistics\n")
+            0 -> return
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
 }
