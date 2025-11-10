c718653ca5222c25ecdf184e705ac3588aaaa932
Anton <new_perspective@mail.ru>
2025-11-11 00:29:21 +0300

    KTB-25 small progress with DatabaseUserDictionary process

--- src/main/kotlin/learningbot/DictionaryDataSource.kt
+++ src/main/kotlin/learningbot/DictionaryDataSource.kt
@@ -1,6 +1,7 @@
 package learningbot
 
 import learningbot.trainer.DICTIONARY_SOURCE_DB
+import learningbot.trainer.DICTIONARY_SOURCE_TXT
 import learningbot.trainer.model.Word
 import java.io.File
 import java.sql.DriverManager
@@ -9,34 +10,12 @@ import java.sql.SQLException
 
 fun main() {
 
-    val dictionary = loadDictionary()
+//updateDictionary(File(DICTIONARY_SOURCE_TXT))
+    val dictionary = DatabaseUserDictionary(11112222)
+    dictionary.loadDictionary().forEach { println(it) }
+
 
 }
-
-fun loadDictionary(): List<Word> {
-    val dictionary = mutableListOf<Word>()
-    DriverManager.getConnection("jdbc:sqlite:$DICTIONARY_SOURCE_DB")
-        .use { connection ->
-            connection.createStatement()
-                .use { statement ->
-                    val result = statement.executeQuery(
-                        """
-                        SELECT words.origin, words.translate
-                        FROM words
-                    """.trimIndent()
-                    )
-                    while (result.next()) {
-                        val word = Word(
-                            origin = result.getString("origin"),
-                            translate = result.getString("translate")
-                        )
-                        dictionary.add(word)
-                    }
-                }
-        }
-    return dictionary
-}
-
 
 fun updateDictionary(wordsFile: File) {
     try {
@@ -44,19 +23,21 @@ fun updateDictionary(wordsFile: File) {
             .use { connection ->
                 connection.createStatement()
                     .use { statement ->
-                        statement.executeUpdate(
-                            """
+
+                        val sql = """
                             CREATE TABLE IF NOT EXISTS "words" (
                               "id" INTEGER PRIMARY KEY AUTOINCREMENT,
                               "origin" varchar UNIQUE,
                               "translate" varchar
                             );
                         """.trimIndent()
-                        )
+                        statement.executeUpdate(sql)
                     }
+
                 val insertSQL = """
                             INSERT OR REPLACE INTO words ("origin", "translate") VALUES (?, ?)
-                        """.trimIndent()
+                            """.trimIndent()
+
                 connection.prepareStatement(insertSQL)
                     .use { ps ->
                         wordsFile.forEachLine { row ->
